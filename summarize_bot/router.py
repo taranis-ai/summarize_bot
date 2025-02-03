taranis-ai/natural-language-processing

Add support for multiple models and enhance configuration management

natural_language_processing/router.py
@@ -1,11 +1,12 @@
 from flask import Blueprint, jsonify, request
 from flask.views import MethodView
-from natural_language_processing.nlp import NLPProcessor
+from natural_language_processing.predictor import Predictor
 from natural_language_processing.decorators import api_key_required
+from natural_language_processing.predictor_factory import PredictorFactory
 
 
 class NLPHandler(MethodView):
-    def __init__(self, processor: NLPProcessor):
+    def __init__(self, processor: Predictor):
         super().__init__()
         self.processor = processor
 
@@ -28,18 +29,19 @@ class HealthCheck(MethodView):
 
 
 class ModelInfo(MethodView):
-    def __init__(self, processor: NLPProcessor):
+    def __init__(self, processor: Predictor):
         super().__init__()
         self.processor = processor
 
     def get(self):
-        return jsonify(self.processor.modelinfo())
+        return jsonify(self.processor.modelinfo)
 
 
-def init(app, nlp_processor):
+def init(app):
+    processor = PredictorFactory()
     app.url_map.strict_slashes = False
     ner_bp = Blueprint("ner", __name__)
-    ner_bp.add_url_rule("/", view_func=NLPHandler.as_view("ner", processor=nlp_processor))
+    ner_bp.add_url_rule("/", view_func=NLPHandler.as_view("ner", processor=processor))
     ner_bp.add_url_rule("/health", view_func=HealthCheck.as_view("health"))
-    ner_bp.add_url_rule("/modelinfo", view_func=ModelInfo.as_view("modelinfo", processor=nlp_processor))
+    ner_bp.add_url_rule("/modelinfo", view_func=ModelInfo.as_view("modelinfo", processor=processor))
     app.register_blueprint(ner_bp)

taranis-ai/summarize_bot

summarize_bot/router.py
@@ -1,19 +1,19 @@
 from flask import Flask, Blueprint, jsonify, request
 from flask.views import MethodView
 
-from summarize_bot.summarize import Summarize
+from summarize_bot.predictor_factory import PredictorFactory
+from summarize_bot.predictor import Predictor
 
 
 class SummarizeText(MethodView):
-    def __init__(self, summarizer: Summarize) -> None:
+    def __init__(self, processor: Predictor) -> None:
         super().__init__()
-        self.summarizer = summarizer
+        self.processor = processor
 
     def post(self):
         data = request.get_json()
         text = data.get("text", "")
-        language = data.get("language", "en")
-        summary = self.summarizer.predict(text, language)
+        summary = self.processor.predict(text)
         return jsonify({"summary": summary})
 
 
@@ -22,10 +22,21 @@ class HealthCheck(MethodView):
         return jsonify({"status": "ok"})
 
 
-def init(app: Flask, summarizer: Summarize):
+class ModelInfo(MethodView):
+    def __init__(self, processor: Predictor):
+        super().__init__()
+        self.processor = processor
+
+    def get(self):
+        return jsonify(self.processor.modelinfo)
+
+
+def init(app: Flask):
+    summarizer = PredictorFactory()
     app.url_map.strict_slashes = False
 
     summarize_bp = Blueprint("predict", __name__)
-    summarize_bp.add_url_rule("/", view_func=SummarizeText.as_view("summarize", summarizer=summarizer))
+    summarize_bp.add_url_rule("/", view_func=SummarizeText.as_view("summarize", processor=summarizer))
     summarize_bp.add_url_rule("/health", view_func=HealthCheck.as_view("health"))
+    summarize_bp.add_url_rule("/modelinfo", view_func=ModelInfo.as_view("modelinfo", processor=summarizer))
     app.register_blueprint(summarize_bp)