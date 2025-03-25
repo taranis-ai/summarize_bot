c4a96644c9587a5c86e0885ac53d27622e26e7e0
Lukas Linauer <l.linauer@gmx.at>
2025-03-25 10:00:28 +0100

    Feature/debug logs (#5)
    * Granian app is called app.py, need to use granian app
    
    * Add debug_request decorator for debugging purposes
    
    * Add tag_regex specific for github pull request merge
    
    * use blacksmith runners
    
    ---------
    
    Co-authored-by: Benjamin Akhras <b@akhras.at>

--- summarize_bot/decorators.py
+++ summarize_bot/decorators.py
@@ -5,6 +5,17 @@ from summarize_bot.config import Config
 from summarize_bot.log import logger
 
 
+def debug_request(func):
+    def wrapper(*args, **kwargs):
+        log_str = f"Method: {request.method}, Endpoint: {request.path}, "
+        payload = request.get_json(silent=True)
+        if payload is not None:
+            log_str += f"Payload: {payload}"
+        logger.debug(log_str)
+        return func(*args, **kwargs)
+    return wrapper
+
+
 def api_key_required(fn):
     @wraps(fn)
     def wrapper(*args, **kwargs):
