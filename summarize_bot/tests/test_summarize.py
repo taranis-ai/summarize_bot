a36d186674423f45bd7dbe6119b76c7d9dd29865
Benjamin Akhras <b@akhras.at>
2025-08-30 13:00:04 +0200

    fix tests

--- summarize_bot/tests/test_summarize.py
+++ summarize_bot/tests/test_summarize.py
@@ -1,3 +1,4 @@
+import asyncio
 from summarize_bot.bart_summarize import BartSummarize
 from summarize_bot.pegasus_summarize import PegasusSummarize
 from summarize_bot.config import Config
@@ -5,7 +6,7 @@ from summarize_bot.config import Config
 
 def test_summarize_bart(article: tuple[str, list], bart_model: BartSummarize):
     content, expected = article
-    result = bart_model.predict(content)
+    result = asyncio.run(bart_model.predict(content))
     assert sum(word in result for word in expected) >= 1, f"No expected keywords were found in the summary: {result}"
     assert len(result) < Config.max_length * 2.5, f"Summary exceeds maximum length.: {len(result)} > {Config.max_length}"
     assert len(result) > Config.min_length, f"Summary is shorter than minimum length.: {len(result)} < {Config.min_length}"
@@ -13,7 +14,7 @@ def test_summarize_bart(article: tuple[str, list], bart_model: BartSummarize):
 
 def test_summarize_pegasus(article: tuple[str, list], pegasus_model: PegasusSummarize):
     content, expected = article
-    result = pegasus_model.predict(content)
+    result = asyncio.run(pegasus_model.predict(content))
     assert sum(word in result for word in expected) >= 1, f"No expected keywords were found in the summary: {result}"
     assert len(result) < Config.max_length * 2.5, f"Summary exceeds maximum length.: {len(result)} > {Config.max_length}"
     assert len(result) > Config.min_length, f"Summary is shorter than minimum length.: {len(result)} < {Config.min_length}"
