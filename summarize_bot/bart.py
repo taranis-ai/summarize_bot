f144ae9b45768b6d656e736adddfca7de408fefc
llinauer <l.linauer@gmx.at>
2026-06-09 08:52:16 +0200

    Adapt predict functions to accept news_item lists or plain text; adapt PAYLOAD_SCHEMA

--- summarize_bot/bart.py
+++ summarize_bot/bart.py
@@ -1,6 +1,7 @@
 from transformers import pipeline
 from summarize_bot.config import Config
-
+from typing import Any
+from summarize_bot.utils import build_story_input_text
 
 class Bart:
     model_name = "facebook/bart-large-cnn"
@@ -8,15 +9,19 @@ class Bart:
     def __init__(self):
         self.summarizer = pipeline("summarization", model=self.model_name)
 
-    async def predict(self, text: str) -> dict[str, str]:
-        if not text:
-            raise ValueError("No text to summarize.")
+    async def predict(self, news_items: list[dict[str, Any]] | None = None, text: str | None = None) -> dict[str, str]:
+        if news_items:
+            text_to_summarize = build_story_input_text(news_items)
+        elif text:
+            text_to_summarize = text
+        else:
+            raise ValueError("No input to summarize")
 
-        if len(text) < Config.MAX_LEN:
-            return {"summary": text}
+        if len(text_to_summarize) < Config.MAX_LEN:
+            return {"summary": text_to_summarize}
 
         summary = self.summarizer(
-            text,
+            text_to_summarize,
             max_new_tokens=Config.MAX_LEN,
             num_beams=Config.NUM_BEAMS,
             min_length=Config.MIN_LEN,
--- summarize_bot/pegasus.py
+++ summarize_bot/pegasus.py
@@ -1,6 +1,7 @@
 from transformers import PegasusTokenizer, PegasusForConditionalGeneration
 from summarize_bot.config import Config
-
+from typing import Any
+from summarize_bot.utils import build_story_input_text
 
 class Pegasus:
     model_name = "google/pegasus-xsum"
@@ -9,14 +10,18 @@ class Pegasus:
         self.tokenizer = PegasusTokenizer.from_pretrained(self.model_name)
         self.summarizer = PegasusForConditionalGeneration.from_pretrained(self.model_name)
 
-    async def predict(self, text: str) -> dict[str, str]:
-        if not text:
-            raise ValueError("No text to summarize.")
+    async def predict(self, news_items: list[dict[str, Any]] | None = None, text: str | None = None) -> dict[str, str]:
+        if news_items:
+            text_to_summarize = build_story_input_text(news_items)
+        elif text:
+            text_to_summarize = text
+        else:
+            raise ValueError("No input to summarize")
 
-        if len(text) < Config.MAX_LEN:
-            return {"summary": text}
+        if len(text_to_summarize) < Config.MAX_LEN:
+            return {"summary": text_to_summarize}
 
-        inputs = self.tokenizer(text, truncation=True, padding="longest", return_tensors="pt")
+        inputs = self.tokenizer(text_to_summarize, truncation=True, padding="longest", return_tensors="pt")
         summary_ids = self.summarizer.generate(
             inputs.input_ids,
             max_new_tokens=Config.MAX_LEN,
